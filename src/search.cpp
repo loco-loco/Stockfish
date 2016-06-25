ef2272223fa277398fdae3970f41edd798721abf
loco-loco <loco-loco@users.noreply.github.com>
2016-06-25 16:16:56 -0700

    Stats update 64

--- src/search.cpp
+++ src/search.cpp
@@ -977,7 +977,7 @@ moves_loop: // When in check search starts from here
               r -= 2 * ONE_PLY;
 
           // Decrease/increase reduction for moves with a good/bad history
-          int rHist = (val - 10000) / 20000;
+          int rHist = (val - 20000) / 40000;
           r = std::max(DEPTH_ZERO, r - rHist * ONE_PLY);
 
           Depth d = std::max(newDepth - r, ONE_PLY);
