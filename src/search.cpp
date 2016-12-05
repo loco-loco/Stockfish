5348f44eadf6475214bcc864d074e00e503001c9
loco-loco <loco-loco@users.noreply.github.com>
2016-12-04 20:14:30 -0800

    Decrease cutNode reduction for killer moves

--- src/search.cpp
+++ src/search.cpp
@@ -990,7 +990,7 @@ moves_loop: // When in check search starts from here
           {
               // Increase reduction for cut nodes
               if (cutNode)
-                  r += 2 * ONE_PLY;
+                  r += (1 + (move != ss->killers[0])) * ONE_PLY;
 
               // Decrease reduction for moves that escape a capture. Filter out
               // castling moves, because they are coded as "king captures rook" and
