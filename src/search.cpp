59bce38b883d26c24646f15e949030eae6609a7f
loco-loco <loco-loco@users.noreply.github.com>
2017-01-06 09:55:14 -0800

    MCP for cutNode

--- src/search.cpp
+++ src/search.cpp
@@ -197,8 +197,8 @@ void Search::init() {
 
   for (int d = 0; d < 16; ++d)
   {
-      FutilityMoveCounts[0][d] = int(2.4 + 0.773 * pow(d + 0.00, 1.8));
-      FutilityMoveCounts[1][d] = int(2.9 + 1.045 * pow(d + 0.49, 1.8));
+      FutilityMoveCounts[0][d] = int(2.9 + 0.773 * pow(d + 0.00, 1.8));
+      FutilityMoveCounts[1][d] = int(3.4 + 1.045 * pow(d + 0.49, 1.8));
   }
 }
 
@@ -875,7 +875,7 @@ moves_loop: // When in check search starts from here
                   : pos.gives_check(move);
 
       moveCountPruning =   depth < 16 * ONE_PLY
-                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY];
+                        && moveCount >= FutilityMoveCounts[improving][depth / ONE_PLY] - cutNode;
 
       // Step 12. Extend checks
       if (    givesCheck
