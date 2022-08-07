537b1e18307015fe1759e3762eddead8a09c92f4
mckx00 <mckx00@gmail.com>
2022-08-07 13:56:50 -0700

    Do DeeperSearch in all cases

--- src/search.cpp
+++ src/search.cpp
@@ -1193,8 +1193,8 @@ moves_loop: // When in check, search starts here
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true);
 
           // If the son is reduced and fails high it will be re-searched at full depth
-          doFullDepthSearch = value > alpha && d < newDepth;
           doDeeperSearch = value > (alpha + 78 + 11 * (newDepth - d));
+          doFullDepthSearch = value > alpha && d < newDepth + doDeeperSearch;
           didLMR = true;
       }
       else
