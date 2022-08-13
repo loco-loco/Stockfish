f466f47e69240d0d6920fb2aad0fbbb25d3fdb20
mckx00 <mckx00@gmail.com>
2022-08-13 06:35:56 -0700

    Lower the chances for deeper search in cut nodes

--- src/search.cpp
+++ src/search.cpp
@@ -1189,7 +1189,7 @@ moves_loop: // When in check, search starts here
 
           // If the son is reduced and fails high it will be re-searched at full depth
           doFullDepthSearch = value > alpha && d < newDepth;
-          doDeeperSearch = value > (alpha + 78 + 11 * (newDepth - d));
+          doDeeperSearch = value > (alpha + 71 + 11 * (newDepth - d + cutNode));
           didLMR = true;
       }
       else
