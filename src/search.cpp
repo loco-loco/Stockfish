27d25a9e828bb4e4f938ba0a7aa19f36a10378a5
loco-loco <mckx00@gmail.com>
2023-02-11 09:24:26 -0800

    Saving static eval on TT table after re-evaluating in excluded search or invalid eval

--- src/search.cpp
+++ src/search.cpp
@@ -733,14 +733,14 @@ namespace {
         // excludeMove implies that we had a ttHit on the containing non-excluded search with ss->staticEval filled from TT
         // However static evals from the TT aren't good enough (-13 elo), presumably due to changing optimism context
         // Recalculate value with current optimism (without updating thread avgComplexity)
-        ss->staticEval = eval = evaluate(pos, &complexity);
+        tte->setEval(ss->staticEval = eval = evaluate(pos, &complexity));
     }
     else if (ss->ttHit)
     {
         // Never assume anything about values stored in TT
         ss->staticEval = eval = tte->eval();
         if (eval == VALUE_NONE)
-            ss->staticEval = eval = evaluate(pos, &complexity);
+            tte->setEval(ss->staticEval = eval = evaluate(pos, &complexity));
         else // Fall back to (semi)classical complexity for TT hits, the NNUE complexity is lost
             complexity = abs(ss->staticEval - pos.psq_eg_stm());
 
